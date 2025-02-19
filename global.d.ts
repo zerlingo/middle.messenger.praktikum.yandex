318b3dfb02e902a95dcca98938e2adcd7d2d7a5e
Anton Volokhov <Zerlingo88@gmail.com>
2025-02-19 02:54:36 +0100

    Добавлены декларации файлов для ts

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,19 @@ declare module '*.css' {
     const classes: { [key: string]: string };
     export default classes;
 }
+
+declare module '*.hbs' {
+    export default string;
+}
+
+declare module '*.png' {
+    export default any;
+}
+
+declare module '*.jpg' {
+    export default any;
+}
+
+declare module '*.svg' {
+    export default any;
+}
