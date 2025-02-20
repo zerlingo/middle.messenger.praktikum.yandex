68b33bc9f82851ebcd53c72cdec4fd9542039a20
Anton Volokhov <Zerlingo88@gmail.com>
2025-02-20 21:03:40 +0100

    Вернул прежний файл tsconfig.json

---
+++ global.d.ts
@@ -0,0 +1,28 @@
+declare module '*.css' {
+    const classes: { [key: string]: string };
+    export default classes;
+}
+
+declare module '*.less' {
+    export default string;
+}
+
+declare module '*.hbs' {
+    export default string;
+}
+
+declare module '*.hbs?raw' {
+    export default string;
+}
+
+declare module '*.png' {
+    export default any;
+}
+
+declare module '*.jpg' {
+    export default any;
+}
+
+declare module '*.svg' {
+    export default any;
+}
